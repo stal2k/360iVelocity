9083c266d033c493d4548c3ec39e5dd2dd7518f2
stal2k <stal2k@gmail.com>
2014-08-25 17:06:21 -0400

    Minor tweaks

--- core/views/admin/admin-m360ivelocity-dashboard.php
+++ core/views/admin/admin-m360ivelocity-dashboard.php
@@ -1,7 +1,7 @@
 <div class="wrap">
 	<div class="m360ivelocity">
 		<div class="icon32" id="360i-logo"><br></div>
-		<h2><?php _e( 'Dashboard', M360IVELOCITY_DOMAIN ); ?></h2>
+		<h2><?php _e( '360i Velocity Dashboard', M360IVELOCITY_DOMAIN ); ?></h2>
 		<iframe id="m360ivelocity-iframe" src="http://velocity.mashable.com/login"></iframe>
 	</div>
 </div>
